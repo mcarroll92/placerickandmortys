e9eb5b9ab9278ae2a4ac92c7ae8f10989ce84819
Matthew Carroll <Matthew@Matthews-MacBook.local>
2016-09-03 17:16:32 +0100

    adding more images

--- app.rb
+++ app.rb
@@ -8,11 +8,36 @@ get '/' do
   erb :index
 end
 
-get '/placeholder' do
-  images = ['morty.jpg', 'rick.jpg']
-  placeholders = images.sample
 
-  erb :placeholder, :locals => {
-    :placeholders => placeholders
-}
+get '/placeholder/?:width?/?:height?' do
+  images = ['morty.jpg', 'rick.jpg', 'rickandmorty.png', 'rickandmorty2.png', 'rickandmorty3.jpg']
+  placeholders = images.sample
+  width = ['width']
+  height = ['height']
+
+  if params['width']['height']
+
+    erb :placeholder, :locals => {
+      :placeholders => placeholders,
+      :width => width,
+      :height => height
+      }
+
+  else
+    erb :placeholder, :locals => {
+      :placeholders => placeholders
+    }
+  end
 end
+
+# get '/placeholder/:width/:height' do |w, h|
+#   width = "#{w}"
+#   height = "#{h}"
+#
+#
+#   erb :placeholder_resize, :locals => {
+#     :width => width
+#     :height => height
+# }
+#
+# end
